43deafb2ec9a5f20bfb7be6386feb99c65794324
Shah Md Hasibur Rahaman Hasib <106367768+Hasib355@users.noreply.github.com>
2022-08-11 22:14:10 +0600

    Add files via upload

---
+++ C Language/Problem No - 1004.c
@@ -0,0 +1,12 @@
+ // Beecrowd Solution : C Programming Language
+ // Problem No : 1004
+
+   #include <stdio.h>
+    int main()
+    {
+         int x,y;
+         scanf("%d%d", &x, &y);
+         printf("PROD = %d\n", x * y);
+         return 0;
+    }
+
