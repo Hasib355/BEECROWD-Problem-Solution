96ee805b431d0ce0c938ce8c9a78e115b203ceba
Shah Md Hasibur Rahaman Hasib <106367768+Hasib355@users.noreply.github.com>
2022-08-11 22:35:10 +0600

    Add files via upload

---
+++ C Language/Problem No - 1011.c
@@ -0,0 +1,12 @@
+ // Beecrowd Solution : C Programming Language
+ // Problem No : 1011
+
+    #include <stdio.h>
+    #include <math.h>
+    int main()
+    {
+         int a;
+         scanf("%i", &a);
+         printf("VOLUME = %.3lf\n", (4 * 3.14159 * pow(a, 3)) / 3);
+         return 0;
+      }
