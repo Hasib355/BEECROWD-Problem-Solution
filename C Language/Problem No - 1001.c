b3dd280bf41d40eca9a228f6cee58476ee6d2320
Shah Md Hasibur Rahaman Hasib <106367768+Hasib355@users.noreply.github.com>
2022-08-11 22:04:23 +0600

    Add files via upload

---
+++ C Language/Problem No - 1001.c
@@ -0,0 +1,12 @@
+ // Beecrowd Solution : C Programming Language
+ // Problem No : 1001
+
+ #include<stdio.h>
+    int main()
+    {
+        int A,B,X;
+        scanf("%d %d",&A,&B);
+        X = A + B;
+        printf("X = %d\n",X);
+        return 0;
+    }
