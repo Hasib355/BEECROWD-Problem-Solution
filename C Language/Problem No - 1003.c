3f114d614429a142d0ec686052e2a457342a42e5
Shah Md Hasibur Rahaman Hasib <106367768+Hasib355@users.noreply.github.com>
2022-08-11 22:05:25 +0600

    Add files via upload

---
+++ C Language/Problem No - 1003.c
@@ -0,0 +1,13 @@
+ // Beecrowd Solution : C Programming Language
+ // Problem No : 1003
+
+#include <stdio.h>
+    int main()
+    {
+         int x;
+         int y;
+         scanf("%d", &x);
+         scanf("%d", &y);
+         printf("SOMA = %d\n", x + y);
+        return 0;
+    }
