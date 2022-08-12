5198bc427aed6d959f08b37e6586525b8ce2c775
Shah Md Hasibur Rahaman Hasib <106367768+Hasib355@users.noreply.github.com>
2022-08-12 22:20:56 +0600

    Add files via upload

---
+++ C Language/Problem No - 1017.c
@@ -0,0 +1,11 @@
+// Beecrowd Solution : C Programming Language
+ // Problem No : 1017
+
+#include <stdio.h>
+    int main()  {
+            double a, b, r;
+            scanf("%lf %lf", &a, &b);
+            r = (a * b)/12;
+            printf("%.3lf\n", r);
+            return 0;
+                }
